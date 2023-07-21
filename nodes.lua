e3b2fc9bf654d5d64621ef87ac9fd99c3af14b01
TechNolaByte <rutabagatime@gmail.com>
2023-07-21 12:21:41 -0400

    add prosperity and inferium ore

--- nodes.lua
+++ nodes.lua
@@ -29,3 +29,108 @@ minetest.register_craft({
 		{"","mystical_agriculture:prudentium_essence",""},
 	}
 })
+
+minetest.register_node("mystical_agriculture:prosperity_essence_ore", {
+	description = "Prosperity Essence Ore",
+	tiles = {"default_stone.png^prosperity_essence_ore.png"},
+	groups = {cracky = 2},
+	light_source = 3,
+	paramtype = "light",
+	drop = "mystical_agriculture:prosperity_essence 2",
+	sounds = default.node_sound_stone_defaults(),
+})
+minetest.register_ore({
+	ore_type       = "scatter",
+	ore            = "mystical_agriculture:prosperity_essence_ore",
+	wherein        = "default:stone",
+	clust_scarcity = 16 * 16 * 16,
+	clust_num_ores = 3,
+	clust_size     = 3,
+	y_min          = -1000,
+	y_max          = 250,
+})
+minetest.register_ore({
+	ore_type       = "scatter",
+	ore            = "mystical_agriculture:prosperity_essence_ore",
+	wherein        = "default:stone",
+	clust_scarcity = 16 * 16 * 16,
+	clust_num_ores = 3,
+	clust_size     = 3,
+	y_min          = 3000,
+	y_max          = 6000,
+})
+
+if(minetest.get_modpath("nether")~=nil) then  
+	minetest.register_node("mystical_agriculture:nether_prosperity_essence_ore", {
+		description = "Nether Prosperity Ore",
+		tiles = {"nether_rack.png^prosperity_essence_ore.png"},
+		groups = {cracky = 2},
+		light_source = 5,
+		paramtype = "light",
+		drop = "mystical_agriculture:chaos_essence 4",
+		sounds = default.node_sound_stone_defaults(),
+	})
+	minetest.register_ore({
+		ore_type       = "scatter",
+		ore            = "mystical_agriculture:nether_prosperity_essence_ore",
+		wherein        = "nether:rack",
+		clust_scarcity = 10 * 10 * 10,
+		clust_num_ores = 3,
+		clust_size     = 3,
+		y_min          = -30000,
+		y_max          = -20000,
+	})
+end
+
+
+minetest.register_node("mystical_agriculture:inferium_essence_ore", {
+	description = "Inferium Essence Ore",
+	tiles = {"default_stone.png^inferium_essence_ore.png"},
+	groups = {cracky = 2},
+	light_source = 3,
+	paramtype = "light",
+	drop = "mystical_agriculture:inferium_essence 2",
+	sounds = default.node_sound_stone_defaults(),
+})
+minetest.register_ore({
+	ore_type       = "scatter",
+	ore            = "mystical_agriculture:inferium_essence_ore",
+	wherein        = "default:stone",
+	clust_scarcity = 16 * 16 * 16,
+	clust_num_ores = 3,
+	clust_size     = 3,
+	y_min          = -1000,
+	y_max          = 250,
+})
+minetest.register_ore({
+	ore_type       = "scatter",
+	ore            = "mystical_agriculture:inferium_essence_ore",
+	wherein        = "default:stone",
+	clust_scarcity = 16 * 16 * 16,
+	clust_num_ores = 3,
+	clust_size     = 3,
+	y_min          = 3000,
+	y_max          = 6000,
+})
+
+if(minetest.get_modpath("nether")~=nil) then  
+	minetest.register_node("mystical_agriculture:nether_inferium_essence_ore", {
+		description = "Nether Prosperity Ore",
+		tiles = {"nether_rack.png^inferium_essence_ore.png"},
+		groups = {cracky = 2},
+		light_source = 5,
+		paramtype = "light",
+		drop = "mystical_agriculture:chaos_essence 4",
+		sounds = default.node_sound_stone_defaults(),
+	})
+	minetest.register_ore({
+		ore_type       = "scatter",
+		ore            = "mystical_agriculture:nether_inferium_essence_ore",
+		wherein        = "nether:rack",
+		clust_scarcity = 10 * 10 * 10,
+		clust_num_ores = 3,
+		clust_size     = 3,
+		y_min          = -30000,
+		y_max          = -20000,
+	})
+end
